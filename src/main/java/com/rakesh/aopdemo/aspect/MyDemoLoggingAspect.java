96df25b56b9d01df99888187f35b9c3a45837c22
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-12-04 16:23:08 -0500

    running the @Before twice

--- src/main/java/com/rakesh/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/rakesh/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,11 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
     // this is where we add all of our related advices for logging
-
-    // let's start with an @Before advice
-
+    // let's start with a @Before advice
     @Before("execution(public void addAccount())")
     public void beforeAddAccountAdvice(){
-        System.out.println("\n ======>>>> Executing @Before advice on addAccount");
+        System.out.println("\n======>>>> Executing @Before advice on addAccount()");
     }
 }
