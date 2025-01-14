d6d2371413dbc68ace4842f29ae7088acc984ccc
RakeshkumarThera <rakeshkumarthera@gmail.com>
2025-01-14 15:38:12 -0500

    Added new method to AccountDAO findAccounts

--- src/main/java/com/rakesh/aopdemo/dao/AccountDAO.java
+++ src/main/java/com/rakesh/aopdemo/dao/AccountDAO.java
@@ -2,7 +2,11 @@ package com.rakesh.aopdemo.dao;
 
 import com.rakesh.aopdemo.Account;
 
+import java.util.List;
+
 public interface AccountDAO {
+
+    List<Account> findAccounts();
     void addAccount(Account theAccount, boolean vipFlag);
 
     boolean doWork();
--- src/main/java/com/rakesh/aopdemo/dao/AccountDAOImpl.java
+++ src/main/java/com/rakesh/aopdemo/dao/AccountDAOImpl.java
@@ -3,12 +3,32 @@ package com.rakesh.aopdemo.dao;
 import com.rakesh.aopdemo.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class AccountDAOImpl implements AccountDAO {
 
     private String name;
     private String serviceCode;
 
+
+    @Override
+    public List<Account> findAccounts() {
+        List<Account> myAccounts = new ArrayList<>();
+
+        // create sample accounts
+        Account temp1 = new Account("Ravi", "Gold");
+        Account temp2 = new Account("Suseela", "Diamond");
+        Account temp3 = new Account("Reshma", "Platinum");
+
+        // add them to our accounts list
+        myAccounts.add(temp1);
+        myAccounts.add(temp2);
+        myAccounts.add(temp3);
+
+        return myAccounts;
+    }
 
     @Override
     public void addAccount(Account theAccount, boolean vipFlag) {
